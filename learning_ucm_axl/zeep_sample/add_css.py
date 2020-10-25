a6e8c61f0b551c2b13d222e0f5fb15625ef4f4f7
dpnetca <git@dpnet.ca>
2020-10-25 16:52:05 -0600

    rename create css to add css, added remove css

---
+++ learning_ucm_axl/zeep_sample/add_css.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+"""
+Get a list of all calling search spaces
+
+Author: Denis Pointer
+Website: dpnet.ca
+"""
+
+import os
+import sys
+
+from requests import Session
+from requests.auth import HTTPBasicAuth
+
+from zeep import Client, Transport
+from zeep.plugins import HistoryPlugin
+from zeep.exceptions import Fault
+
+from dotenv import load_dotenv
+
+# Disable insecure SSL warnings
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+load_dotenv()
+
+# Load Environmental Variable
+wsdl = os.getenv("WSDL_FILE")
+username = os.getenv("UCM_USERNAME")
+password = os.getenv("UCM_PASSWORD")
+ucm_pub_url = f'https://{os.getenv("UCM_PUB_ADDRESS")}:8443/axl/'
+
+# Create Session, do not verify certificate, enable basic auth
+session = Session()
+session.verify = False
+session.auth = HTTPBasicAuth(username, password)
+
+# create transport with our session and 10 second timeout
+transport = Transport(session=session, timeout=10)
+
+history = HistoryPlugin()
+
+# crete the Zeep client
+client = Client(wsdl, transport=transport, plugins=[history])
+
+# create the service proxy pointint to our UCM
+service = client.create_service(
+    binding_name="{http://www.cisco.com/AXLAPIService/}AXLAPIBinding",
+    address=ucm_pub_url,
+)
+
+
+add_css_data = {
+    "name": "dp-test-css",
+    "description": "Denis test CSS with AXL",
+    "members": {"member": []},
+}
+
+try:
+    add_css_resp = service.addCss(add_css_data)
+    print(add_css_resp)
+except Fault as f:
+    print(f"Unable to add CSS: {f}")
+    sys.exit(1)
+
+try:
+    get_css_resp = service.getCss(uuid=add_css_resp["return"])
+    print(get_css_resp)
+except Fault as f:
+    print(f"Unable to get CSS: {f}")
+    sys.exit(1)
